a15fc553429c05e1cdff50f7265dfd9b7ce5f149
radekBednarik <radek.bednarik@tesena.com>
2020-10-18 17:32:42 +0200

    graph works

--- czech_covid19_data_visualization/graphs.py
+++ czech_covid19_data_visualization/graphs.py
@@ -2,19 +2,16 @@ from typing import Any, Dict
 
 import dash_core_components as dcc
 import pandas as pd
-from pandas import DataFrame
 import plotly.graph_objects as go
+from pandas import DataFrame
+from plotly.subplots import make_subplots
 
 
 def graph_vertical_bar(data: Dict[str, Any], graph_number: int = 1) -> Any:
     df: DataFrame = DataFrame.from_records(data)
 
-    return dcc.Graph(
-        id=f"barchart_{graph_number}",
-        figure=go.Figure(
-            data=[
-                go.Bar(x=df.iloc[:, 0], y=df.iloc[:, 1]),
-                go.Scatter(x=df.iloc[:, 0], y=df.iloc[:, 2]),
-            ]
-        ),
-    )
+    fig: Any = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Bar(x=df.iloc[:, 0], y=df.iloc[:, 1]), secondary_y=False)
+    fig.add_trace(go.Scatter(x=df.iloc[:, 0], y=df.iloc[:, 2]), secondary_y=True)
+
+    return dcc.Graph(id=f"barchart_{graph_number}", figure=fig)
